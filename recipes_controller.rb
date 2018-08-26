437ed01d0107194261de1086836507939e0b4310
Waldecy <waldecyfa@gmail.com>
2018-08-26 11:14:49 -0300

    Add files via upload

---
+++ recipes_controller.rb
@@ -0,0 +1,45 @@
+class RecipesController < ApplicationController
+
+	def index
+	  @recipes = Recipe.all
+	end
+
+	def show
+      @recipe = Recipe.find(params[:id]) 
+     
+    end
+
+    def edit
+       @recipe = Recipe.find(params[:id])
+
+    end
+
+    def update
+    	@recipe=Recipe.find(params[:id])
+    	recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+    	@recipe.update(recipe_params)
+        redirect_to @recipe
+
+    end
+
+    def new 
+      @recipe=Recipe.new
+
+    end
+
+    def create
+        recipe_params = params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+        @recipe = Recipe.new(recipe_params)
+        @recipe.save
+        redirect_to @recipe
+
+    end
+
+    def destroy
+        @recipe=Recipe.find(params[:id])
+        @recipe.destroy
+        redirect_to recipes_url
+        
+  end
+
+end
